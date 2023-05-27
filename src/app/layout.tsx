5f75da4c18f94d479116a7ad4f8106a4bd631a57
Saqlen Raza Mehdi <saqlenrazamehdi@gmail.com>
2023-05-27 22:46:11 +0500

    Added GA Script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Space_Grotesk } from "next/font/google";
 import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
+import Script from "next/script";
+
 const space_grotesk = Space_Grotesk({
   weight: ["500", "400", "600", "700"],
   subsets: ["latin"],
@@ -24,6 +26,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-TQ8L18XFFY"
+        />
+
+        <Script strategy="afterInteractive" id="google-analytics-script">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TQ8L18XFFY');
+`}
+        </Script>
+      </head>
       <body className={space_grotesk.className}>
         <Nav />
         <Hero />
