a3c64a00d52528b059bf48b244241865ed9fa9a5
Saqlen Raza Mehdi <saqlenrazamehdi@gmail.com>
2023-07-05 12:39:16 +0500

    resized the images

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,9 +11,7 @@ export default function Hero() {
     <section className={styles.container}>
       <div className={styles.banner}>
         <Image
-          src={
-            "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-          }
+          src={"/projects/banner.jpg"}
           fill
           style={{ objectFit: "cover" }}
           alt={"Banner AI"}
